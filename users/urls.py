f0a9db26909bbff5b1ba066f7896a89acc8cdf0b
ByronPiedrahita <byronpiedrahita.programmer@gmail.com>
2020-12-02 16:43:41 -0500

    Detail View and List View

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,5 @@
-"""Users URLs."""
-
 # Django
 from django.urls import path
-from django.views.generic import TemplateView
 
 # View
 from users import views
@@ -10,32 +7,33 @@ from users import views
 
 urlpatterns = [
 
-    # Posts
-    path(
-        route='profile/<str:username>/',
-        view=TemplateView.as_view(template_name='users/detail.html'),
-        name='detail'
-    ),
-
     # Management
     path(
-        route = 'login/', 
-        view = views.login_view, 
+        route='login/',
+        view=views.login_view,
         name='login'
     ),
     path(
-        route = 'logout/', 
-        view = views.logout_view, 
+        route='logout/',
+        view=views.logout_view,
         name='logout'
     ),
     path(
-        route = 'signup/', 
-        view = views.signup, 
-        name ='signup'
+        route='signup/',
+        view=views.signup,
+        name='signup'
     ),
     path(
-        route = 'me/profile/', 
-        view = views.update_profile, 
-        name='update_profile'
+        route='me/profile/',
+        view=views.update_profile,
+        name='update'
     ),
+
+    # Posts
+    path(
+        route='<str:username>/',
+        view=views.UserDetailView.as_view(),
+        name='detail'
+    )
+
 ]
